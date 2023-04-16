9495ff8e03d1741b7ee774ae7c7114c6d51bae50
Aman Varshney <av.amanvarshney11@gmail.com>
2023-04-16 19:23:49 +0530

    3d text

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Navbar() {
     return (
-        <nav className={""}>
+        <nav className={"z-50"}>
             <ul className={""}>
                 <li className={""}>
                     <Link href={"/"}>Home</Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,19 @@ import Scene from './Scene'
 export default function Home() {
   return (
       <>
-          <main className="z-10">
-              <h1 className={""}>Aman Varshney</h1>
+          <main className="flex flex-row">
+              <div className={""}>
+                  <h1 className={""}>Aman Varshney</h1>
+              </div>
+              <div className={"w-1/2"}>
+                  <Suspense fallback={null}>
+                      <Canvas shadows flat linear>
+                          <Scene />
+                          <OrbitControls />
+                      </Canvas>
+                  </Suspense>
+              </div>
           </main>
-          <div className={"fixed h-screen w-screen top-0 left-0 z-0"}>
-              <Suspense fallback={null}>
-                  <Canvas shadows flat linear>
-                      <Scene />
-                      <OrbitControls />
-                  </Canvas>
-              </Suspense>
-          </div>
       </>
   )
 }
