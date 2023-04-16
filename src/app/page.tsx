eca2c5b8222cd3c9bcd0aaa470a910dce8fcb211
Aman Varshney <av.amanvarshney11@gmail.com>
2023-04-16 14:03:47 +0530

    3d text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
-// import Image from 'next/image'
-// import { Inter } from 'next/font/google'
-//
-// const inter = Inter({ subsets: ['latin'] })
+"use client";
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import Scene from './Scene'
 
 export default function Home() {
   return (
-    <main className="">
-      <h1>Aman Varshney</h1>
-    </main>
+      <>
+          <main className="z-10">
+              <h1 className={""}>Aman Varshney</h1>
+          </main>
+          <div className={"fixed h-screen w-screen top-0 left-0 z-0"}>
+              <Suspense fallback={null}>
+                  <Canvas shadows flat linear>
+                      <Scene />
+                      <OrbitControls />
+                  </Canvas>
+              </Suspense>
+          </div>
+      </>
   )
 }
