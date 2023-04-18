6d6e1493d623246cd8c79115b80ca77f5cd7ad5a
Aman Varshney <av.amanvarshney11@gmail.com>
2023-04-18 20:20:49 +0530

    content in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 export default function Home() {
   return (
       <main className="flex w-full justify-end h-screen z-10 absolute top-0 left-0 ">
-          <div className={"w-1/2 flex justify-center items-center"}>
-              <h1 className={""}>Aman Varshney</h1>
+          <div className={"w-1/2 flex flex-col items-center  p-10 "}>
+              <div className={"w-fit"}>
+                  <h1 className={"text-3xl"}>Aman Varshney</h1>
+                  <p>Hi </p>
+              </div>
           </div>
       </main>
   )
