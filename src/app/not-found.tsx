dfc48a82b5538be53fdfc4320f304a4d8540e77e
Aman Varshney <av.amanvarshney11@gmail.com>
2023-04-26 22:09:31 +0530

    not found page update

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ import sadMeme from 'public/sad-crying-face.gif'
 export default function NotFound() {
     return (
         <div className={"h-full"}>
-            <Image className={"w-full h-auto"} src={sadMeme} alt={"Sad"} />
+            <Image className={" p-10 w-full h-auto"} src={sadMeme} alt={"Sad"} />
             <h1 className={"text-lg text-center font-bold my-6"}>Sorry Page Not Found</h1>
         </div>
     );
