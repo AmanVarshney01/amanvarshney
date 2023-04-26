adc2a5c48fe41f2c4507f6981074b926754014c5
Aman Varshney <av.amanvarshney11@gmail.com>
2023-04-26 22:32:08 +0530

    reload button

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,14 @@
+"use client"
 import Image from "next/image";
 import sadMeme from 'public/sad-crying-face.gif'
 
 export default function NotFound() {
     return (
         <div className={"h-full"}>
-            <Image className={" p-10 w-full h-auto"} src={sadMeme} alt={"Sad"} />
+            <Image loading={"lazy"} className={" p-10 w-full h-auto"} src={sadMeme} alt={"Sad"} />
             <div className={"flex flex-col justify-center items-center gap-4"}>
                 <h1 className={"text-lg font-bold "}>Sorry Page Not Found</h1>
-                <p>Try Refreshing Page</p>
+                <button className={"underline text-blue-600"} onClick={() => window.location.reload()}>Refresh Page</button>
             </div>
         </div>
     );
