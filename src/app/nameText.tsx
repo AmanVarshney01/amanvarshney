66bd839eeb54d6f91e0e01b24678c44523970a80
Aman Varshney <av.amanvarshney11@gmail.com>
2023-04-27 02:40:07 +0530

    changed 3d text color

--- src/app/nameText.tsx
+++ src/app/nameText.tsx
@@ -6,10 +6,10 @@ import {Text3D} from '@react-three/drei'
 
 export default function NameText() {
     return (
-        <Text3D castShadow receiveShadow position={[-7,-5,0]} font={"./Lato_Regular.json"} size={1} lineHeight={1} letterSpacing={0}>
+        <Text3D castShadow receiveShadow position={[-6.8,-5,0]} font={"./Lato_Regular.json"} size={1} lineHeight={1} letterSpacing={0}>
             AMAN VARSHNEY
             <meshStandardMaterial
-                color={"#f26a8d"}
+                color={"#3C6997"}
                 // color={"white"}
             />
         </Text3D>
