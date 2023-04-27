66a5b1c45b1e1cfaa7255600116d452dc0926604
Aman Varshney <av.amanvarshney11@gmail.com>
2023-04-27 19:39:47 +0530

    added info in projects

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
                 <ul className={"leading-8 text-justify list-disc flex flex-col gap-4"}>
                     <li>Hi, I'm Aman Varshney, a first-year student pursuing a Bachelor of Technology in Computer Science and Engineering at GLA University.</li>
                     <li>I was born and brought up in Chandausi, Uttar Pradesh, India. However, I have spent most of my life in Delhi, where I grew up and lived for 17 years. Recently, I have relocated to Ghaziabad.</li>
-                    <li>I was a member of Geeks for Geeks Student Chapter for 5 months.</li>
+                    {/*<li>I was a member of Geeks for Geeks Student Chapter for 5 months.</li>*/}
                 </ul>
             </div>
         </main>
