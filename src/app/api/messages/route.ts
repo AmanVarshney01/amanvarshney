4e8e3eb6689680a73b7a12916df64af26a949e24
Aman Varshney <av.amanvarshney11@gmail.com>
2023-05-04 02:06:56 +0530

    fix

--- src/app/api/messages/route.ts
+++ src/app/api/messages/route.ts
@@ -1,5 +1,27 @@
 import { sql } from "@vercel/postgres"
 
+
+// export async function GET() {
+//     try {
+//         const { rows } = await sql`SELECT * FROM entries;`
+//
+//         const body = rows.map((row) => ({
+//             id: row.id,
+//             name: row.name,
+//             message: row.message,
+//             date: row.created_at
+//         }))
+//
+//         return new Response(JSON.stringify(body), {
+//             headers: { "content-type": "application/json" },
+//             status: 200
+//         })
+//     } catch (error) {
+//         console.log(error)
+//         return new Response('Something went wrong.', { status: 500 })
+//     }
+// }
+
 export async function POST(request: Request) {
     const {name, message} = await request.json()
 
--- src/app/guestbook/page.tsx
+++ src/app/guestbook/page.tsx
@@ -1,6 +1,8 @@
 import { sql } from "@vercel/postgres";
 import MessageForm from "@/app/guestbook/MessageForm";
 
+export const revalidate = 60;
+
 export default async function page(): Promise<JSX.Element> {
     const { rows } = await sql`SELECT * from entries`;
     return (
