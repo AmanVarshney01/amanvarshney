a364d4b22578c61a6e67bf9ac4122a21237e2f6a
Aman Varshney <av.amanvarshney11@gmail.com>
2023-05-04 02:25:13 +0530

    styling fixes

--- src/app/guestbook/MessageForm.tsx
+++ src/app/guestbook/MessageForm.tsx
@@ -24,7 +24,7 @@ export default function MessageForm() {
     }
 
     return (
-        <form onSubmit={handleSubmit} className={"flex flex-row w-full justify-between gap-2"}>
+        <form onSubmit={handleSubmit} className={"flex lg:flex-row flex-col w-full justify-between gap-2"}>
             <input required type="text" name="name" id="name" placeholder={"Your Name"} className={"flex-3 p-1 rounded-sm"} />
             <input required type="text" name="message" id="message" placeholder={"Your Message"} className={"flex-1 p-1 rounded-sm"} />
             <button type="submit" className={"p-2 rounded-sm bg-[#121212] dark:bg-[#f2f2f2] text-white dark:text-black hover:scale-105"}>Submit</button>
