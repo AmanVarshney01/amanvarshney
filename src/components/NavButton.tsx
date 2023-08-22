32844a708b60d4d5d5fb04b96cdef308e45a54fc
Aman Varshney <av.amanvarshney11@gmail.com>
2023-08-22 21:47:25 +0530

    navbar animation fix

--- src/components/NavButton.tsx
+++ src/components/NavButton.tsx
@@ -7,7 +7,7 @@ import {usePathname} from "next/navigation";
 export default function NavButton({title, href}: {title: string, href: string}) {
     const path = usePathname()
     return (
-        <m.li whileHover={{scale: 1.1}} whileTap={{scale: 0.95}} className={""}>
+        <m.li whileHover={{scale: 1.1}} className={""}>
             <Link className={"relative transition ease-in duration-200 font-medium text-lg"} href={href}>
                 {href === path && (<m.span layoutId="nav-underline" className={"absolute left-0 top-full block h-[2px] dark:bg-white bg-black w-full rounded-2xl"} />)}
                 {title}
