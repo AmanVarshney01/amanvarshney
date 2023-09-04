b4a87b6e35f349e09c690b4e920aebaf555bb407
Aman Varshney <av.amanvarshney11@gmail.com>
2023-09-05 00:13:12 +0530

    changed about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
               <div className={"flex flex-col gap-4 h-full"}>
                   <Photo />
                   <h1 className={"text-3xl"}>Aman Varshney</h1>
-                  <p className={"leading-8 text-justify"}>I am a first year <strong>B.Tech CSE student</strong> with a keen interest in web development. My current focus is on building projects using Next.js, Tailwind. As a continuous learner, I am always striving to improve my skills and create exciting new web experiences.</p>
+                  <p className={"leading-8 text-justify"}>I am a second year <strong>B.Tech CSE student</strong> with a keen interest in web development. My current focus is on building projects using Next.js, Tailwind. As a continuous learner, I am always striving to improve my skills and create exciting new web experiences.</p>
                   {/*<p></p>*/}
                   <div className={"flex items-center justify-center w-full"}>
                       <Marquee text={"Next.js Tailwind React Svelte Javascript Typescript Python C/C++"} />
